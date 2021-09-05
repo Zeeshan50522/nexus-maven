e79553c1ea8c308634c1e681a607888a4b6e4b54
muhammad zeeshan <zeeshanwaheed50522@gmail.com>
2021-09-06 04:06:42 +0500

    jenkins maven

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,7 +6,7 @@ package com.mycompany.app;
 public class App
 {
 
-    private final String message = "Hello World Fron Jenkins";
+    private final String message = "Hello World From Jenkins";
 
     public App() {}
 
